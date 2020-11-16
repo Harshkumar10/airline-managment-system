fbdd081eb66073240aedf7bbdeeb05d4a8edf536
Shivraj1999712 <74522472+Shivraj1999712@users.noreply.github.com>
2020-11-16 12:16:31 +0530

    Updated for last time

--- images.php
+++ images.php
@@ -4,9 +4,27 @@ require('d.php');
 ?>
 <html>
 <head>
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="s.css">
 </head>
 <body>
+    <header>
+		<div><h1 id="logo"><img src="https://cdn.dribbble.com/users/2761722/screenshots/7138526/image.png" height="80" width="80"/></h1></div>
+		<div><h1 id="company-name">SHS Flight Booking</h1></div>
+	</header><br>
+	
+	<nav>
+		<div id="main">
+		    <div><button onclick="window.location='./H.php' ">Home</button></div>
+			<div><button onclick="window.location='./i.php' ">Airport</button></div>
+			<div><button onclick="window.location='./v.php' ">Flight</button></div>
+			<div><button onclick="window.location='./Ticket.php' ">Ticket</button></div>
+			<div><button onclick="window.location='./lo.php' ">Logout</button></div>
+            <div><button onclick="window.location='./Ticket1.php' ">Ticket Prices</button></div>
+			<div><button onclick="window.location='./Profile.php' ">Profile</button></div>
+		</div>
+	</nav>
+
+
 <?php
 
 	if(isset($_POST['submit'])){
@@ -35,5 +53,8 @@ require('d.php');
    <input type="submit" name="submit" value="Submit"/>
  </form>
  <?php }?>
+ 
+
+
  </body>
  </html>
