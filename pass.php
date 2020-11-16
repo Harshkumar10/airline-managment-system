5760ef5822c2226a5eda639897840b3ff15ac6ea
Shivraj1999712 <74522472+Shivraj1999712@users.noreply.github.com>
2020-11-16 12:21:11 +0530

    Updated for last time

--- pass.php
+++ pass.php
@@ -1,11 +1,27 @@
 <html>
 <head>
 <title>Password recovery</title>
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="s.css">
 </head>
 <body>
-<h1 style="margin-left:535px;"><img src="https://cdn.dribbble.com/users/2761722/screenshots/7138526/image.png" height="100" width="100"/></h1>
-<h1 style="margin-left:450px;">SHS Flight Booking</h1>
+    <header>
+		<div><h1 id="logo"><img src="https://cdn.dribbble.com/users/2761722/screenshots/7138526/image.png" height="80" width="80"/></h1></div>
+		<div><h1 id="company-name">SHS Flight Booking</h1></div>
+	</header><br>
+	
+	<nav>
+		<div id="main">
+		    <div><button onclick="window.location='./H.php' ">Home</button></div>
+			<div><button onclick="window.location='./i.php' ">Airport</button></div>
+			<div><button onclick="window.location='./v.php' ">Flight</button></div>
+			<div><button onclick="window.location='./Ticket.php' ">Ticket</button></div>
+			<div><button onclick="window.location='./lo.php' ">Logout</button></div>
+            <div><button onclick="window.location='./Ticket1.php' ">Ticket Prices</button></div>
+			<div><button onclick="window.location='./Profile.php' ">Profile</button></div>
+		</div>
+	</nav>
+
+
 <?php
 session_start();
 require('d.php');
@@ -35,6 +51,43 @@ header("Location: m.php");
 </form>
 </div>
 <?php } ?>
+
+		<div id="chain">
+			<div>
+				<h5 style="color:white;">About this site</h5><br>
+				<a href='About.php' class='ca'>About us</a><br><br>
+				<a href='Privacy.php' class='ca'>Privacy Policy</a><br><br>
+				<a href='Feedback.php' class='ca'>Feedback</a>
+			</div>
+
+			<div>
+			<h5 style="color:white;">Popular Domestic Route</h5><br>
+			<a href='Search1.php' class='ca'>Chennai Mumbai</a><br><br>
+			<a href='Search1.php' class='ca'>Delhi Mumbai</a><br><br>
+			<a href='Search1.php' class='ca'>Delhi Goa</a>
+			</div>
+
+			<div>
+			<h5 style="color:white;">Popular International Route</h5><br>
+			<a href='Search1.php' class='ca'>Delhi Frankfurt</a><br><br>
+			<a href='Search1.php' class='ca'>Mumbai SanFrancisco</a><br><br>
+			<a href='Search1.php' class='ca'>Delhi SanFrancisco</a>
+			</div>
+
+			<div>
+			<h5 style="color:white;">Popular Indian Airlines</h5><br>
+			<a href='https://www.goindigo.in/' class='ca'>Indigo airlines</a><br><br>
+			<a href='http://www.airindia.in/' class='ca'>Air India airlines</a><br><br>
+			<a href='https://www.airasia.com/en/gb' class='ca'>Air asia airlines</a>
+			</div>
+
+			<div>
+			<h5 style="color:white;">International Airlines</h5><br>
+			<a href='https://www.autohotel.org/?gclid=Cj0KCQiAy579BRCPARIsAB6QoIZyjX0SHRJQXLRknyt2wnEEN3wkbaIZi8qyGlnWiFezrVP_a-hTg5EaAkN3EALw_wcB' class='ca'>Frankfurt airlines</a><br><br>
+			<a href='https://flyaerohp.com/?gclid=Cj0KCQiAy579BRCPARIsAB6QoIa7AOPY6K9y2-S4alUGN5AulYpdwjWo1xq7cDI8EWEnguu8Y1Yxmi4aAtjMEALw_wcB' class='ca'>Houston airlines</a><br><br>
+			<a href='https://www.skyscanner.co.in/flights-to/dfw/airlines-that-fly-to-dallas-fort-worth-international-airport.html' class='ca'>Fort Worth airlines</a>
+			</div>
+
 </body>
 </html>
 
