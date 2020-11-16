e0b6689105acff3f77b2355f7d6a2f3bd323a250
Shivraj1999712 <74522472+Shivraj1999712@users.noreply.github.com>
2020-11-16 12:20:15 +0530

    Updated for last time

--- m1.php
+++ m1.php
@@ -4,7 +4,7 @@ require('d.php');
 ?>
 <html>
 <head>
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="s.css">
 <title>Mail Ticket</title>
 </head>
 <body>
@@ -23,12 +23,15 @@ require('d.php');
    $class= $_POST["class"];
    $age=$_POST["age"];
    $sex=$_POST["sex"];
+   $cookie_name="place";
+   $cookie_value=$dest;
+   setcookie($cookie_name, $cookie_value, time() + (86400 * 15), "/");
    $q1="SELECT * FROM flight WHERE SOURCE='$source' and DESTINATION='$dest'";
    $r1=mysqli_query($con,$q1) or die(mysql_error());
    $ro1=mysqli_fetch_assoc($r1);
    if(!$ro1)
    {
-	 echo"No Flights found For This route Please Check for route Via Mumbai or Delhi or local route";
+	 echo"No Flights found For This route Please Check for route Via Mumbai or Delhi or local route or fill the details properly";
    }
    else
    {
@@ -68,5 +71,44 @@ require('d.php');
     }
     }
 ?>
+<br><br>
 </body>
+
+		<div id="chain">
+			<div>
+				<h5 style="color:white;">About this site</h5><br>
+				<a href='About.php' class='ca'>About us</a><br><br>
+				<a href='Privacy.php' class='ca'>Privacy Policy</a><br><br>
+				<a href='Feedback.php' class='ca'>Feedback</a>
+			</div>
+
+			<div>
+			<h5 style="color:white;">Popular Domestic Route</h5><br>
+			<a href='Search1.php' class='ca'>Chennai Mumbai</a><br><br>
+			<a href='Search1.php' class='ca'>Delhi Mumbai</a><br><br>
+			<a href='Search1.php' class='ca'>Delhi Goa</a>
+			</div>
+
+			<div>
+			<h5 style="color:white;">Popular International Route</h5><br>
+			<a href='Search1.php' class='ca'>Delhi Frankfurt</a><br><br>
+			<a href='Search1.php' class='ca'>Mumbai SanFrancisco</a><br><br>
+			<a href='Search1.php' class='ca'>Delhi SanFrancisco</a>
+			</div>
+
+			<div>
+			<h5 style="color:white;">Popular Indian Airlines</h5><br>
+			<a href='https://www.goindigo.in/' class='ca'>Indigo airlines</a><br><br>
+			<a href='http://www.airindia.in/' class='ca'>Air India airlines</a><br><br>
+			<a href='https://www.airasia.com/en/gb' class='ca'>Air asia airlines</a>
+			</div>
+
+			<div>
+			<h5 style="color:white;">International Airlines</h5><br>
+			<a href='https://www.autohotel.org/?gclid=Cj0KCQiAy579BRCPARIsAB6QoIZyjX0SHRJQXLRknyt2wnEEN3wkbaIZi8qyGlnWiFezrVP_a-hTg5EaAkN3EALw_wcB' class='ca'>Frankfurt airlines</a><br><br>
+			<a href='https://flyaerohp.com/?gclid=Cj0KCQiAy579BRCPARIsAB6QoIa7AOPY6K9y2-S4alUGN5AulYpdwjWo1xq7cDI8EWEnguu8Y1Yxmi4aAtjMEALw_wcB' class='ca'>Houston airlines</a><br><br>
+			<a href='https://www.skyscanner.co.in/flights-to/dfw/airlines-that-fly-to-dallas-fort-worth-international-airport.html' class='ca'>Fort Worth airlines</a>
+			</div>
+
+		</div>
 </html>
