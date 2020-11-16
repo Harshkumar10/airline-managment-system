cb7b34758eaec48c49feb964da2ea0ead7478384
Shivraj1999712 <74522472+Shivraj1999712@users.noreply.github.com>
2020-11-16 12:12:37 +0530

    Updated for last time

--- Ticket.php
+++ Ticket.php
@@ -4,12 +4,28 @@
 <html>
 <head>
 <title>Ticket Registration</title>
-<link rel="stylesheet" href="style.css">
+<link rel="stylesheet" href="s.css">
 </head>
 <body>
-<h1 style="text-align:center;"><img src="https://cdn.dribbble.com/users/2761722/screenshots/7138526/image.png" height="100" width="100"/></h1>
-<h1 style="text-align:center;">SHS Flight Booking</h1>
+     <header>
+		<div><h1 id="logo"><img src="https://cdn.dribbble.com/users/2761722/screenshots/7138526/image.png" height="80" width="80"/></h1></div>
+		<div><h1 id="company-name">SHS Flight Booking</h1></div>
+	</header><br>
+	
+	<nav>
+		<div id="main">
+		    <div><button onclick="window.location='./H.php' ">Home</button></div>
+			<div><button onclick="window.location='./i.php' ">Airport</button></div>
+			<div><button onclick="window.location='./v.php' ">Flight</button></div>
+			<div><button onclick="window.location='./Ticket.php' ">Ticket</button></div>
+			<div><button onclick="window.location='./lo.php' ">Logout</button></div>
+            <div><button onclick="window.location='./Ticket1.php' ">Ticket Prices</button></div>
+			<div><button onclick="window.location='./Profile.php' ">Profile</button></div>
+		</div>
+	</nav>
+
 <div>
+<h2>Ticket booking</h2>
 <form  method="post" action="m1.php">
 <p><input type="text" name="username" placeholder="Enter username" required /></p>
 <select name="apname" style="width:198px;height:40px;">
@@ -33,9 +49,51 @@
   <option title="Ahmedabad" value="Sardar Vallabhbhai Patel International Airport" >Sardar Vallabhbhai Patel International Airport</option>
   <option title="Surat" value="Surat International Airport" >Surat International Airport</option>
   <option title="Tampa" value="Tampa International Airport" >Tampa International Airport</option>
-</select><br>
-<p><input type="text" name="source"  placeholder="Enter Source" required /></p>
-<p><input type="text" name="destination"  placeholder="Enter destination" required /></p>
+</select><br><br>
+<select name="source" style="width:198px;height:40px;">
+  <option value="Calicut">Calicut</option>
+  <option value="Chandigarh">Chandigarh</option>
+  <option value="Chennai">Chennai</option>
+  <option value="Mumbai">Mumbai</option>
+  <option value="Goa">Goa</option>
+  <option value="Fort Worth">Fort Worth</option>
+  <option value="Indore">Indore</option>
+  <option value="Frankfurt">Frankfurt</option>
+  <option value="Houston">Houston</option>
+  <option value="Delhi">Delhi</option>
+  <option value="New York City">New York City</option>
+  <option value="Bangalore">Bangalore</option>
+  <option value="Varanasi">Varanasi</option>
+  <option value="Louisville">Louisville</option>
+  <option value="Kolkata">Kolkata</option>
+  <option value="Pune">Pune</option>
+  <option value="San Francisco">San Francisco</option>
+  <option value="Ahmedabad">Ahmedabad</option>
+  <option value="Surat">Surat</option>
+  <option value="Tampa">Tampa</option>
+</select><br><br>
+<select name="destination" style="width:198px;height:40px;">
+  <option value="Calicut">Calicut</option>
+  <option value="Chandigarh">Chandigarh</option>
+  <option value="Chennai">Chennai</option>
+  <option value="Mumbai">Mumbai</option>
+  <option value="Goa">Goa</option>
+  <option value="Fort Worth">Fort Worth</option>
+  <option value="Indore">Indore</option>
+  <option value="Frankfurt">Frankfurt</option>
+  <option value="Houston">Houston</option>
+  <option value="Delhi">Delhi</option>
+  <option value="New York City">New York City</option>
+  <option value="Bangalore">Bangalore</option>
+  <option value="Varanasi">Varanasi</option>
+  <option value="Louisville">Louisville</option>
+  <option value="Kolkata">Kolkata</option>
+  <option value="Pune">Pune</option>
+  <option value="San Francisco">San Francisco</option>
+  <option value="Ahmedabad">Ahmedabad</option>
+  <option value="Surat">Surat</option>
+  <option value="Tampa">Tampa</option>
+</select><br><br>
 <p><input type="date"  name="date"  placeholder="Enter date of travelling" required /></p>
 <input type="radio" name="class" value="ECONOMY" required>ECONOMY CLASS<br>
 <input type="radio" name="class" value="BUSINESS" required>BUSINESS CLASS<br>
@@ -46,5 +104,41 @@
 <p><input name="submit" type="submit"  value="Book" /></p>
 </form>
 </div>
+		<div id="chain">
+			<div>
+				<h5 style="color:white;">About this site</h5><br>
+				<a href='About.php' class='ca'>About us</a><br><br>
+				<a href='Privacy.php' class='ca'>Privacy Policy</a><br><br>
+				<a href='Feedback.php' class='ca'>Feedback</a>
+			</div>
+
+			<div>
+			<h5 style="color:white;">Popular Domestic Route</h5><br>
+			<a href='Search1.php' class='ca'>Chennai Mumbai</a><br><br>
+			<a href='Search1.php' class='ca'>Delhi Mumbai</a><br><br>
+			<a href='Search1.php' class='ca'>Delhi Goa</a>
+			</div>
+
+			<div>
+			<h5 style="color:white;">Popular International Route</h5><br>
+			<a href='Search1.php' class='ca'>Delhi Frankfurt</a><br><br>
+			<a href='Search1.php' class='ca'>Mumbai SanFrancisco</a><br><br>
+			<a href='Search1.php' class='ca'>Delhi SanFrancisco</a>
+			</div>
+
+			<div>
+			<h5 style="color:white;">Popular Indian Airlines</h5><br>
+			<a href='https://www.goindigo.in/' class='ca'>Indigo airlines</a><br><br>
+			<a href='http://www.airindia.in/' class='ca'>Air India airlines</a><br><br>
+			<a href='https://www.airasia.com/en/gb' class='ca'>Air asia airlines</a>
+			</div>
+
+			<div>
+			<h5 style="color:white;">International Airlines</h5><br>
+			<a href='https://www.autohotel.org/?gclid=Cj0KCQiAy579BRCPARIsAB6QoIZyjX0SHRJQXLRknyt2wnEEN3wkbaIZi8qyGlnWiFezrVP_a-hTg5EaAkN3EALw_wcB' class='ca'>Frankfurt airlines</a><br><br>
+			<a href='https://flyaerohp.com/?gclid=Cj0KCQiAy579BRCPARIsAB6QoIa7AOPY6K9y2-S4alUGN5AulYpdwjWo1xq7cDI8EWEnguu8Y1Yxmi4aAtjMEALw_wcB' class='ca'>Houston airlines</a><br><br>
+			<a href='https://www.skyscanner.co.in/flights-to/dfw/airlines-that-fly-to-dallas-fort-worth-international-airport.html' class='ca'>Fort Worth airlines</a>
+			</div>
+
 </body>
 </html>
