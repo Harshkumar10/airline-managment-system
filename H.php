f740fe3d150f59eea6a90e3b9d6f135afd6b4580
Shivraj1999712 <74522472+Shivraj1999712@users.noreply.github.com>
2020-11-16 11:59:42 +0530

    Update H.php
    -m updated for same code

--- H.php
+++ H.php
@@ -2,22 +2,22 @@
 <html lang="en">
 <head>
 	<title>Home</title>
-	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="s.css">
 </head>
 <body>
 	<header>
-		<div><h1 class="logo"><img src="https://cdn.dribbble.com/users/2761722/screenshots/7138526/image.png" height="80" width="80"/></h1></div>
-		<div><h1 class="company-name">SHS Flight Booking</h1></div>
-	</header>
+		<div><h1 id="logo"><img src="https://cdn.dribbble.com/users/2761722/screenshots/7138526/image.png" height="80" width="80"/></h1></div>
+		<div><h1 id="company-name">SHS Flight Booking</h1></div>
+	</header><br>
 
 	<nav>
-		<div class="main">
+		<div id="main">
+		    <div><button onclick="window.location='./H.php' ">Home</button></div>
 			<div><button onclick="window.location='./i.php' ">Airport</button></div>
 			<div><button onclick="window.location='./v.php' ">Flight</button></div>
 			<div><button onclick="window.location='./Ticket.php' ">Ticket</button></div>
 			<div><button onclick="window.location='./lo.php' ">Logout</button></div>
-
-			<div><button onclick="window.location='./Ticket1.php' ">Ticket Prices</button></div>
+            <div><button onclick="window.location='./Ticket1.php' ">Ticket Prices</button></div>
 			<div><button onclick="window.location='./Profile.php' ">Profile</button></div>
 		</div>
 	</nav>
@@ -45,7 +45,7 @@
 		if(isset($_SESSION['username']))
 		echo $_SESSION['username']; 
 	?>
-	<!-- <marquee  scrollamount="15" behavior="alternate">
+	<br><br><marquee  scrollamount="15" behavior="alternate">
 		<img src="https://image.shutterstock.com/image-vector/concept-illustration-template-book-your-600w-1197774070.jpg" height="350" width="500" />
 		<img src="https://image.shutterstock.com/image-photo/booking-flight-travel-traveler-search-600w-520314439.jpg" height="350" width="500" />
 		<img src="https://image.shutterstock.com/image-illustration/air-travelling-concept-passport-boarding-600w-1154346064.jpg" height="350" width="500" />
@@ -54,7 +54,16 @@
 		<img src="https://image.shutterstock.com/image-photo/air-ticket-booking-on-smartphone-600w-1164143659.jpg" height="350" width="500" />
 		<img src="https://image.shutterstock.com/image-photo/flight-over-new-york-city-600w-313140194.jpg" height="350" width="500"/>
 		<img src="https://image.shutterstock.com/image-photo/flight-booking-ticket-icon-graphic-600w-617741123.jpg" height="350" width="500"/>
-	</marquee><br><br><br> -->
+	</marquee><br><br><br> 
+	<h2 style="text-align:left;">Places you may like</h2>
+	<?php
+	if(isset($_COOKIE["place"])){
+		echo $_COOKIE["place"];
+	}
+	else
+	{ echo"we will Show the places of your Choice";
+	}
+	 ?>
 	
 	<h2 style="text-align:center;">Most Searched Destinations</h2>
 	<div style="margin-left:150px;" >
@@ -71,37 +80,33 @@
 		<h2 style="color:black;">Why SHS?</h2>
 		<p style="color:black;">SHS is a prominent market player in the Indian Online travel sector and is the most popular air ticket booking platform that helps you save money on any flight bookings. In addition to providing the best online flight tickets deals, SHS enables customers to save money on fares every time they book tickets. You might visit our website or download the SHS mobile app for booking your tickets on the go. Exclusive coupons and attractive discounts available. The secure payment channel and user-friendly interface helps user to enjoy a seamless flight ticket booking experience.</p>
 	</div>
-	<footer>
+	
 		<div id="chain">
 			<div>
 				<h5 style="color:white;">About this site</h5><br>
 				<a href='About.php' class='ca'>About us</a><br><br>
 				<a href='Privacy.php' class='ca'>Privacy Policy</a><br><br>
-				<a href='Search.php' class='ca'>Cookies Policy</a><br><br>
-				<a href='Search.php' class='ca'>Feedback</a>
+				<a href='Feedback.php' class='ca'>Feedback</a>
 			</div>
 
 			<div>
 			<h5 style="color:white;">Popular Domestic Route</h5><br>
-			<a href='Search.php' class='ca'>Chennai Mumbai</a><br><br>
-			<a href='Search.php' class='ca'>Delhi Mumbai</a><br><br>
-			<a href='Search.php' class='ca'>Mumbai Goa</a><br><br>
-			<a href='Search.php' class='ca'>Delhi Goa</a>
+			<a href='Search1.php' class='ca'>Chennai Mumbai</a><br><br>
+			<a href='Search1.php' class='ca'>Delhi Mumbai</a><br><br>
+			<a href='Search1.php' class='ca'>Delhi Goa</a>
 			</div>
 
 			<div>
 			<h5 style="color:white;">Popular International Route</h5><br>
-			<a href='Search.php' class='ca'>Delhi Frankfurt</a><br><br>
-			<a href='Search.php' class='ca'>Mumbai SanFrancisco</a><br><br>
-			<a href='Search.php' class='ca'>Mumbai Frankfurt</a><br><br>
-			<a href='Search.php' class='ca'>Delhi SanFrancisco</a>
+			<a href='Search1.php' class='ca'>Delhi Frankfurt</a><br><br>
+			<a href='Search1.php' class='ca'>Mumbai SanFrancisco</a><br><br>
+			<a href='Search1.php' class='ca'>Delhi SanFrancisco</a>
 			</div>
 
 			<div>
 			<h5 style="color:white;">Popular Indian Airlines</h5><br>
 			<a href='https://www.goindigo.in/' class='ca'>Indigo airlines</a><br><br>
 			<a href='http://www.airindia.in/' class='ca'>Air India airlines</a><br><br>
-			<a href='https://www.spicejet.com/' class='ca'>Spicejet airlines</a><br><br>
 			<a href='https://www.airasia.com/en/gb' class='ca'>Air asia airlines</a>
 			</div>
 
@@ -109,11 +114,9 @@
 			<h5 style="color:white;">International Airlines</h5><br>
 			<a href='https://www.autohotel.org/?gclid=Cj0KCQiAy579BRCPARIsAB6QoIZyjX0SHRJQXLRknyt2wnEEN3wkbaIZi8qyGlnWiFezrVP_a-hTg5EaAkN3EALw_wcB' class='ca'>Frankfurt airlines</a><br><br>
 			<a href='https://flyaerohp.com/?gclid=Cj0KCQiAy579BRCPARIsAB6QoIa7AOPY6K9y2-S4alUGN5AulYpdwjWo1xq7cDI8EWEnguu8Y1Yxmi4aAtjMEALw_wcB' class='ca'>Houston airlines</a><br><br>
-			<a href='https://www.seaplane.com/' class='ca'>San Francisco airlines</a><br><br>
 			<a href='https://www.skyscanner.co.in/flights-to/dfw/airlines-that-fly-to-dallas-fort-worth-international-airport.html' class='ca'>Fort Worth airlines</a>
 			</div>
 
 		</div>
-	</footer>
 </body>
 </html>
