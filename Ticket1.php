da283540014a9edf0a9b28fe3efe91ece219fbc6
Shivraj1999712 <74522472+Shivraj1999712@users.noreply.github.com>
2020-11-30 09:29:20 +0530

    Final update for project evaluation

--- Ticket1.php
+++ Ticket1.php
@@ -6,6 +6,8 @@
 <head>
 	<title>Tickets Price</title>
 	<link rel="stylesheet" href="s.css">
+	<link rel="icon" href="https://cdn.dribbble.com/users/2761722/screenshots/7138526/image.png" height="100" width="100" type="image/gif">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 <body>
    <header>
@@ -121,8 +123,8 @@
 			<div>
 			<h5 style="color:white;">Popular International Route</h5><br>
 			<a href='Search1.php' class='ca'>Delhi Frankfurt</a><br><br>
-			<a href='Search1.php' class='ca'>Mumbai SanFrancisco</a><br><br>
-			<a href='Search1.php' class='ca'>Delhi SanFrancisco</a>
+			<a href='Search1.php' class='ca'>Calicut SanFrancisco</a><br><br>
+			<a href='Search1.php' class='ca'>Delhi NewYorkCity</a>
 			</div>
 
 			<div>
